210ea5097d2ac8b9cd9577f16df78979484fcd98
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-08 12:12:06 +0300

    Use shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            SignIn
-            </button>
+          <Button>
+            Sign In
+          </Button>
         </SignInButton>
       </SignedOut>
 
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <Button variant={"secondary"}>
+        Click me
+      </Button>
     </div>
   );
 }
