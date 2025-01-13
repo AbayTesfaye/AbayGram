7d4da57cff976d31bd0dd9b733fb8552bb619262
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-13 12:36:10 +0300

    Auth and DB sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 export default async function Home() {
-  await prisma.
   return (
     <div className="m-4">
       <h1>Home page content!</h1>
