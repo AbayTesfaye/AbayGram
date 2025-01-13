6f0778d2fedaf7e87e7dfd6a1798b18cda9d487e
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-13 11:02:00 +0300

    setup prisma client to interact with database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-export default function Home() {
+export default async function Home() {
+  await prisma.
   return (
     <div className="m-4">
       <h1>Home page content!</h1>
