4af7d72df06d398c20bf1fddf22d51e7172e7c04
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-16 17:40:14 +0300

    Show the UI of user post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
+import { getPosts } from "@/action/post.action";
+import { getDbUserId } from "@/action/user.action";
 import CreatePost from "@/components/CreatePost";
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
+import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
-import { prisma } from "@/lib/prisma";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
+  const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-       <div className="lg:col-span-6">
-        <CreatePost />
-       </div>
-       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        <WhoToFollow />
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+            // <div>Post Card</div>
+          ))}
         </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
